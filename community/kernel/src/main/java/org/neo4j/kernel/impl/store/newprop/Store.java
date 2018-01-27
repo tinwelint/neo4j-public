74d821312ada49090b5c1e0fbb724bfd48a048e5
Mattias Finné <mattias@neotechnology.com>
2018-01-27 19:10:30 +0100

    Added some javadoc to Store

--- community/kernel/src/main/java/org/neo4j/kernel/impl/store/newprop/Store.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/store/newprop/Store.java
@@ -36,6 +36,19 @@ import static org.neo4j.kernel.impl.store.newprop.UnitCalculation.EFFECTIVE_UNIT
 import static org.neo4j.kernel.impl.store.newprop.UnitCalculation.offsetForId;
 import static org.neo4j.kernel.impl.store.newprop.UnitCalculation.pageIdForRecord;
 
+/**
+ * A very simple store abstraction on top of a {@link PagedFile}. Provides {@link PageCursor} and allocates ids
+ * to other parties that wants to implement some sort of actual store.
+ *
+ * Access, whether it being read or write, is visitor-based to get the good parts of {@link PageCursor#shouldRetry()}
+ * checking as well as bounds/cursor exception checking. Also id --> pageId/offset calculations.
+ *
+ * This store assumes 64 byte record units with the first unit in each page reserved for {@link Header} data,
+ * which contains inUse and startUnit information about all the other records.
+ *
+ * What this class is really lacking is free-list management. Generally using this store you'd probably want to allocate
+ * records of one or more consecutive units so a normal dumb free-list doesn't do very well. i.e. TODO free-list
+ */
 class Store implements Closeable
 {
     static final long SPECIAL_ID_SHOULD_RETRY = -2;
@@ -50,6 +63,14 @@ class Store implements Closeable
         this.storeFile = pageCache.map( new File( directory, name ), pageSize, CREATE, WRITE, READ );
     }
 
+    /**
+     * Allocates a number of consecutive units in this store. Allocation generally prioritizes all units residing
+     * on the same page.
+     *
+     * @param units number of 64 byte units to allocate.
+     * @return the start id of this record.
+     * @throws IOException on page cache allocation error.
+     */
     long allocate( int units ) throws IOException
     {
         if ( units > EFFECTIVE_UNITS_PER_PAGE )
@@ -82,16 +103,46 @@ class Store implements Closeable
         return storeFile.io( 0, flags );
     }
 
+    /**
+     * Allocates a {@link PageCursor} capable of making changes to this store. The returned instances should be passed
+     * into {@link #access(long, PageCursor, RecordVisitor)} when reading from and/or writing to this store.
+     *
+     * @return a {@link PageCursor} capable of reading from and writing to pages.
+     * @throws IOException on page cursor I/O error.
+     */
     PageCursor writeCursor() throws IOException
     {
         return cursor( PagedFile.PF_SHARED_WRITE_LOCK );
     }
 
+    /**
+     * Allocates a {@link PageCursor} only capable of reading data from this store. The returned instances should be passed
+     * into {@link #access(long, PageCursor, RecordVisitor)} when reading from this store.
+     *
+     * @return a {@link PageCursor} capable of reading from pages in this store.
+     * @throws IOException on page cursor I/O error.
+     */
     PageCursor readCursor() throws IOException
     {
         return cursor( PagedFile.PF_SHARED_READ_LOCK );
     }
 
+    /**
+     * Accesses this store using an allocated {@link PageCursor}, with one of the {@link #writeCursor()} or {@link #readCursor()}
+     * methods. The access is driven by the {@link RecordVisitor} which can (a) carry data into the store
+     * and (b) retrieve data from the stare and carry it back to the caller.
+     *
+     * Consistency, i.e. {@link PageCursor#shouldRetry()} and bounds checking is managed in this call and mostly invisible
+     * to the visitor.
+     *
+     * {@link Header} is consulted to get number of units for the given record id, information which is passed into
+     * {@link RecordVisitor#accept(PageCursor, long, int)}, after visitor has been {@link RecordVisitor#initialize(PageCursor) initialized}.
+     *
+     * @param id record id, as handed out by {@link #allocate(int)}.
+     * @param cursor {@link PageCursor} as handed out by {@link #writeCursor()} or {@link #readCursor()}.
+     * @param visitor {@link RecordVisitor} with user-specific logic accessing this store.
+     * @throws IOException on {@link PageCursor} I/O error.
+     */
     void access( long id, PageCursor cursor, RecordVisitor visitor ) throws IOException
     {
         long pageId = pageIdForRecord( id );
@@ -131,12 +182,20 @@ class Store implements Closeable
         }
     }
 
+    /**
+     * Close this store so that no more access can be served.
+     *
+     * @throws IOException on {@link PageCache} I/O error.
+     */
     @Override
     public void close() throws IOException
     {
         storeFile.close();
     }
 
+    /**
+     * Logic for accessing this store.
+     */
     interface RecordVisitor
     {
         /**
@@ -146,8 +205,18 @@ class Store implements Closeable
         void initialize( PageCursor cursor );
 
         /**
+         * Called by {@link Store#access(long, PageCursor, RecordVisitor)} after {@link #initialize(PageCursor)} have
+         * been called and the proper ceremonies around {@link PageCursor} have been made. This visitor is now allowed
+         * to read or write (depending on cursor of course) what ever it wants to read/write.
+         * For reading {@link PageCursor#shouldRetry()} may result in this method being called multiple times for a single
+         * read, so the implementation must be able to handle that.
+         *
+         * @param cursor {@link PageCursor} retrieved from {@link Store#readCursor()} or {@link Store#writeCursor()}.
+         * @param startId record id retrieved from {@link Store#allocate(int)}.
+         * @param units number of units that makes up this record.
          * @return -1 if no more pages should be accessed, non-negative if the access should
          * continue in a new place, another id.
+         * @throws IOException on {@link PageCursor} I/O error.
          */
         long accept( PageCursor cursor, long startId, int units ) throws IOException;
     }
